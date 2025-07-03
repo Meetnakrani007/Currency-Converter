f52e212f3bea0122776545e0c525355a0dab4217
Meet Nakrani <nakranimeet2005@gmail.com>
2025-07-03 13:07:42 +0530

    Update_2.O

---
+++ app.js
@@ -0,0 +1,80 @@
+const BASE_URL = "https://open.er-api.com/v6/latest";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+const switchIcon = document.getElementById("switch-icon");
+
+for (let select of dropdowns) {
+  for (let currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || isNaN(amtVal) || amtVal <= 0) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+  const URL = `${BASE_URL}/${fromCurr.value}`;
+  try {
+    let response = await fetch(URL);
+    if (!response.ok) throw new Error("Network response was not ok.");
+    let data = await response.json();
+    let rate = data.rates[toCurr.value];
+    let finalAmount = (amtVal * rate).toFixed(2);
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+  } catch (error) {
+    msg.innerText = "Something went wrong! Please try again later. 😞";
+    console.error("Error fetching exchange rate:", error);
+  }
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateFlag(fromCurr);
+  updateFlag(toCurr);
+  updateExchangeRate();
+});
+
+switchIcon.addEventListener("click", () => {
+  switchIcon.classList.toggle("rotate");
+
+  let temp = fromCurr.value;
+  fromCurr.value = toCurr.value;
+  toCurr.value = temp;
+
+  updateFlag(fromCurr);
+  updateFlag(toCurr);
+});
